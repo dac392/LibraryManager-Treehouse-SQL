5954faa89620a7101dccc78fda981408b98d55d8
dac392 <dac392@scarletmail.rutgers.edu>
2022-07-24 11:29:08 -0400

    can search for a subset of books, allows for partial matches

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const db = require('./models/index');
-// const This = db.sequelize;
+// const {Op} = db.Sequelize;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,79 @@
 var express = require('express');
 var router = express.Router();
 const Book = require('../models').Book;
+const {Op} = require('../models/index').Sequelize; 
+
+function scan(str){
+  const regex = /^(title|author|genre|year): ".+"( \^ (title|author|genre|year): ".+")?$/gi;
+  console.log();
+  if(!regex.test(str)){
+    error = new Error(`Oh no! your search "${str}" is not properly formatted. Remember, you can use " ^ " to represent AND, and to use the format:\t <key> "<value>"`);
+    throw error;
+  }
+}
+function parse(str){
+  const arr = str.split(" ^ ");
+  let title = "%";
+  let author = "%";
+  let genre = "%";
+  let year = "%";
+  for( let i = 0; i < arr.length; i++){
+    let query = arr[i].split(":");
+    let key = query[0];
+    let value = query[1].replace(/\s+/g, ' ').trim();
+    switch(key){
+      case "title":
+        title = (value.replace(/"+/g, ''));
+        break;
+      case "author":
+        author = (value.replace(/"+/g, ''));
+        break;
+      case "genre":
+        genre = (value.replace(/"+/g, ''));
+        break;
+      case "year":
+        year = (value.replace(/"+/g, ''));
+        break;
+    }
+  }
+  return {title, author, genre, year};
+}
+
 
 /* GET home page. */
 router.get('/', async function(req, res) {
   const books = await Book.findAll();
   res.render('index',{title:"Books", books});
+});
+
+/* Search */
+router.post('/search', async function(req, res){
+  try{
+    const str = req.body.search.toLowerCase();
+    scan(str);
+    const search = parse(str);
+    const books =  await Book.findAll({
+      where: {
+        [Op.and]:[
+          {
+            title:{[Op.like]: `%${search.title}%`},
+            author:{[Op.like]: `%${search.author}%`},
+            genre:{[Op.like]: search.genre},
+            year:{[Op.like]: search.year},
+          }
+        ]
+      }
+    });
+    // console.dir(books);
+    res.render('index',{title:"Search results", books, home: true});
+  }catch(error){
+    // console.dir(error.name);
+    // console.dir(error.message);
+    const books = await Book.findAll();
+    res.render(`index`, { title: "Books", books, errors: [error]});
+    
+  }
+
 });
 
 router.get('/books/new', async (req, res)=>{
